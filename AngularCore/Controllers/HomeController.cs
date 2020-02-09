969e57b64feec487df5370b72d4b2b78ffe85281
sipakov <dimasipakov@gmail.com>
2020-02-09 19:40:11 +0300

    refactor angular + mvc

---
+++ AngularCore/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using AngularCore.Infrastructure;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AngularCore.Controllers
+{
+    public class HomeController : BaseController
+    {
+        public HomeController(IHttpContextAccessor httpContextAccessor) : base(httpContextAccessor)
+        {
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
